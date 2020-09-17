b448faf970cb697d3e9875e4aba42a0b3d476d19
Christian <cisr918@gmail.com>
2020-09-17 02:05:24 -0400

    v0.0.2
    - Added health bar
    - Added beginning code for Q, W, E, and R abilities
    - Increased player movement speed

---
+++ Assets/Abilities.cs
@@ -0,0 +1,87 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Abilities : MonoBehaviour {
+    [Header("Q Ability")]
+    public Image qAbilityImage;
+    public float qCooldown = 0f;
+
+    [Header("W Ability")]
+    public Image wAbilityImage;
+    public float wCooldown = 0f;
+
+    [Header("E Ability")]
+    public Image eAbilityImage;
+    public float eCooldown = .5f;
+
+    [Header("R Ability")]
+    public Image rAbilityImage;
+    public float rCooldown = 30f;
+
+    void Start() {
+        qAbilityImage.fillAmount = 0;
+        wAbilityImage.fillAmount = 0;
+        eAbilityImage.fillAmount = 0;
+        rAbilityImage.fillAmount = 0;
+    }
+
+    // Update all abilities
+    void Update() {
+        qAbility();
+        wAbility();
+        eAbility();
+        rAbility();
+    }
+
+    void qAbility() {
+        if (Input.GetKey(KeyCode.Q) && qAbilityImage.fillAmount == 0) {
+            qAbilityImage.fillAmount = 1; // Cooldown started
+            return;
+        }
+        if (qAbilityImage.fillAmount > 0) {
+            qAbilityImage.fillAmount -= 1 / qCooldown * Time.deltaTime;
+
+            if (qAbilityImage.fillAmount <= 0)
+                qAbilityImage.fillAmount = 0; // Cooldown finished
+        }
+    }
+
+    void wAbility() {
+        if (Input.GetKey(KeyCode.W) && wAbilityImage.fillAmount == 0) {
+            wAbilityImage.fillAmount = 1; // Cooldown started
+            return;
+        }
+        if (wAbilityImage.fillAmount > 0) {
+            wAbilityImage.fillAmount -= 1 / wCooldown * Time.deltaTime;
+
+            if (wAbilityImage.fillAmount <= 0)
+                wAbilityImage.fillAmount = 0; // Cooldown finished
+        }
+    }
+
+    void eAbility() {
+        if (Input.GetKey(KeyCode.E) && eAbilityImage.fillAmount == 0) {
+            eAbilityImage.fillAmount = 1; // Cooldown started
+            return;
+        }
+        if (eAbilityImage.fillAmount > 0) {
+            eAbilityImage.fillAmount -= 1 / eCooldown * Time.deltaTime;
+
+            if (eAbilityImage.fillAmount <= 0)
+                eAbilityImage.fillAmount = 0; // Cooldown finished
+        }
+    }
+
+    void rAbility() {
+        if (Input.GetKey(KeyCode.R) && rAbilityImage.fillAmount == 0) {
+            rAbilityImage.fillAmount = 1; // Cooldown started
+            return;
+        }
+        if (rAbilityImage.fillAmount > 0) {
+            rAbilityImage.fillAmount -= 1 / rCooldown * Time.deltaTime;
+
+            if (rAbilityImage.fillAmount <= 0)
+                rAbilityImage.fillAmount = 0; // Cooldown finished
+        }
+    }
+}
---
+++ Assets/PlayerHealthSlider.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class PlayerHealthSlider : MonoBehaviour {
+
+    void LateUpdate() {
+        transform.LookAt(Camera.main.transform);
+        transform.Rotate(0f, 180f, 0f);
+    }
+}
