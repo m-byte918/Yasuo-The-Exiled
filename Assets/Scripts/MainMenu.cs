1d32dfc0ce89dcea5fa97f5d08e91636e7dbf8a4
Delldor20 <71194035+Delldor20@users.noreply.github.com>
2020-12-02 14:28:58 -0500

    Add files via upload

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public AudioSource firstPart;
+    public AudioSource secondPart;
+    
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        double introDuration = (double)firstPart.clip.samples / firstPart.clip.frequency;
+        double startTime = AudioSettings.dspTime - 0.3;
+        firstPart.PlayScheduled(startTime);
+        secondPart.PlayScheduled(startTime + introDuration);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+}
