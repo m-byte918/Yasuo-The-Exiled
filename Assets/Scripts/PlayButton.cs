3377025b78e7e2b59f16b9d6c3a5172f14312753
Delldor20 <71194035+Delldor20@users.noreply.github.com>
2021-09-19 16:28:41 -0400

    Worked on Title Screen
    Added Title Screen Text
    Changed buttons and Tile Screen UI

---
+++ Assets/Scripts/PlayButton.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayButton : MonoBehaviour
+{
+    public AudioSource voiceLine;
+
+    private void OnMouseOver()
+    {
+        if(Input.GetMouseButtonUp(0))
+        {
+            voiceLine.Play();
+
+            StartCoroutine(WaitForSceneLoad());
+        }
+    }
+
+    private IEnumerator WaitForSceneLoad()
+    {
+        yield return new WaitForSeconds(4);
+        SceneManager.LoadScene(1);
+    }
+}
---
+++ Assets/Scripts/QuitButton.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitButton : MonoBehaviour
+{
+    private void OnMouseOver()
+    {
+        Application.Quit();
+    }
+}
