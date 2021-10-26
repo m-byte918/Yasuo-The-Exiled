cdaa5eab9440bbb947b52f3a71de48de2e345cfa
Christian <cisr918@gmail.com>
2021-10-26 09:25:07 -0400

    Added placeholder shopkeep & shop UI

---
+++ Assets/Scripts/Shopkeep.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Shopkeep : MonoBehaviour {
+    public Canvas shop;
+
+    void OnTriggerEnter(Collider other) {
+        if (other.name != "Player") {
+            return;
+        }
+        shop.gameObject.SetActive(true); // Open shop UI
+    }
+}
