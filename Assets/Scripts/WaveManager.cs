930871778134dc43fccc9aff85b31f7164f98361
Delldor20 <71194035+Delldor20@users.noreply.github.com>
2021-11-11 16:23:45 -0500

    Game Changes
    Added Texture to rocks
    Changed enemy Spawn

--- Assets/Scripts/WaveManager.cs
+++ Assets/Scripts/WaveManager.cs
@@ -6,7 +6,7 @@ public class WaveManager : MonoBehaviour {
     public GameObject enemyPrefab;
     public Canvas playerHealthCanvas;
 
-    public float waveGrowthRate = 1.5f;
+    public float waveGrowthRate = 0.5f;
     public int currentEnemyCount = 0;
     private int currentWaveSize = 1;
     private int currentWave = 0;
@@ -14,21 +14,23 @@ public class WaveManager : MonoBehaviour {
     public void spawnNextWave() {
         ++currentWave;
 
-        GameObject currentWaveTrigger = GameObject.Find("Wave " + currentWave);
-        GameObject nextWaveTrigger = GameObject.Find("Wave " + (currentWave + 1));
+        GameObject currentWaveTrigger = GameObject.Find("SpawnZone " + currentWave);
+        GameObject nextWaveTrigger = GameObject.Find("SpawnZone " + (currentWave + 1));
 
         if (!currentWaveTrigger || !nextWaveTrigger) {
             // No start or end of waves
             return;
         }
         // Scale current wave size by predetermined factor
-        int nextWaveSize = (int)System.Math.Ceiling(currentWaveSize * waveGrowthRate);
+        int nextWaveSize = ++currentWaveSize;
+
+        Debug.Log("Next Wave Size: " + nextWaveSize);
 
         // Enemy spawn bound
         float minX = currentWaveTrigger.transform.position.x - currentWaveTrigger.transform.lossyScale.x / 2f;
         float maxX = currentWaveTrigger.transform.position.x + currentWaveTrigger.transform.lossyScale.x / 2f;
         float minZ = currentWaveTrigger.transform.position.z + currentWaveTrigger.transform.lossyScale.z / 2f;
-        float maxZ = nextWaveTrigger.transform.position.z - nextWaveTrigger.transform.lossyScale.z / 2f;
+        float maxZ = currentWaveTrigger.transform.position.z - currentWaveTrigger.transform.lossyScale.z / 2f;
         float spawnY = GameObject.Find("Player").transform.position.y;
 
         for (int i = 0; i < nextWaveSize; ++i) {
